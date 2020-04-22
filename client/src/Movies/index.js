f962162d9ace5ef4c329aca8bc0b43b69d5958fe
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-04-22 11:36:25 -0700

    project complete

---
+++ client/src/Movies/index.js
@@ -0,0 +1,4 @@
+export {default as Movie} from "./Movie";
+export {default as MovieCard} from "./MovieCard";
+export {default as MovieList} from "./MovieList";
+export {default as SavedList} from "./SavedList";
